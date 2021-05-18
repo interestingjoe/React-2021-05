095e0b74b89c1ca21b724071f1c9a3e4d4c9bc1f
Your Name <you@example.com>
2021-05-17 19:06:48 -0700

    Adding AddTask state.

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header.js'
 import Tasks from './components/Tasks.js'
+import AddTask from './components/AddTask.js'
 import { useState } from 'react'
 
 
 function App() {
+    const [showAddTask, setShowAddTask] = useState(false)
     const [tasks, setTasks] = useState(
         [
             {
@@ -39,6 +41,12 @@ function App() {
         ]
     );
 
+    const addTask = (task) => {
+        const id = Math.floor(Math.random() * 10000) + 1
+        const newTask = {id, ...task}
+        setTasks([...tasks, newTask])
+    }
+
     const deleteTask = (id) => {
         setTasks(
             tasks.filter((task) => task.id !== id)
@@ -57,7 +65,11 @@ function App() {
 
     return (
         <div className="container">
-            <Header title='This is the TITLE'/>
+            <Header title='This is the TITLE' 
+            onAdd={() => setShowAddTask(!showAddTask)} 
+            showAdd={showAddTask}
+            />
+            {showAddTask && <AddTask onAdd={addTask} />}
             {tasks.length > 0 
                 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
                 : 'No tasks to show'
--- hello-world/src/components/Header.js
+++ hello-world/src/components/Header.js
@@ -1,19 +1,17 @@
 import PropTypes from 'prop-types'
 import Button from './Button.js'
 
-const Header = ({ title }) => {
-    const onClick = (e) => {
-        console.log(e.target);
-    }
-
+const Header = ({ title , onAdd, showAdd }) => {
     return (
         <header className='header'>
             <h1 style={headingStyle}>
                 {title}
             </h1>
-            <Button color='yellow' copy='NEW Copy' onClick={onClick} />
-            <Button color='green' copy='Copy 2' onClick={onClick} />
-            <Button color='black' copy='Copy 3' onClick={onClick} />
+            <Button 
+                color={showAdd ? 'red' : 'green'}  
+                copy={showAdd ? 'Close' : 'Add'} 
+                onClick={onAdd} 
+            />
         </header>
     )
 }
